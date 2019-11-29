53090a441cdc8e2dfe48403a2e3a9e7dc41e456b
Mateus Menezes <mateuscoelho2009@bitbucket.org>
2019-11-28 21:37:39 -0300

    Updates

---
+++ precache-manifest.573ec08b9e298b9927835a3a4f72d893.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c973e475d5133bd19dd81e2950257474",
+    "url": "/gba-emu-web/index.html"
+  },
+  {
+    "revision": "a0dd7dc1e89e0fd520fd",
+    "url": "/gba-emu-web/static/css/main.9f855fef.chunk.css"
+  },
+  {
+    "revision": "03c35ab89a90eaed4915",
+    "url": "/gba-emu-web/static/js/2.affa402b.chunk.js"
+  },
+  {
+    "revision": "a0dd7dc1e89e0fd520fd",
+    "url": "/gba-emu-web/static/js/main.ca3323e2.chunk.js"
+  },
+  {
+    "revision": "869f09210fa3c8abb617",
+    "url": "/gba-emu-web/static/js/runtime-main.4a672535.js"
+  },
+  {
+    "revision": "d96f92c9198c33bf00fbb3700ea77e73",
+    "url": "/gba-emu-web/static/media/bg.d96f92c9.png"
+  },
+  {
+    "revision": "3465b6bf3424464024eeec9f84ccd9dd",
+    "url": "/gba-emu-web/static/media/bios.3465b6bf.bin"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/gba-emu-web/static/media/logo.25bf045c.svg"
+  }
+]);
