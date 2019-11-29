2ff744d696b86eade787b1322dbbd37a26e8e828
Luis Henrique <luishenriquesf10@gmail.com>
2019-11-29 18:46:24 -0300

    main page initial version

--- src/App.js
+++ src/App.js
@@ -8,16 +8,34 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Inspired by the project <a href="https://github.com/endrift/gbajs" target="_blank">gbajs</a> which was descontinued.
+
+        This project intends to:
+          <ul>
+            <li>
+            Create a react component that can be used for playing GameBoy Advanced games in browser;
+            </li>
+            <li>
+            Refactor code from <a href="https://github.com/endrift/gbajs" target="_blank">gbajs</a>;
+            </li>
+            <li>
+            Include mobile gameplay support;
+            </li>
+            <li>
+            Develop a platform for playing GBA online.
+            </li>
+          </ul>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          <a
+            className="App-link"
+            href="https://mateuscoelho2009.github.io/gba-emu-web/#/emulator"
+            target="_self"
+            rel="noopener noreferrer"
+          >
+            Run Emulator
+          </a>
+        </p>
       </header>
     </div>
   );
